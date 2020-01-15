ae94ed1a37fcacddd46be7d269e59f04ea67b16b
Oskar Hahn <mail@oshahn.de>
2020-01-15 20:34:54 +0100

    Remove meeting and make ids required

--- internal/keysbuilder/interfaces.go
+++ internal/keysbuilder/interfaces.go
@@ -5,6 +5,5 @@ import "context"
 // Restricter restricts keys. See autoupdate.Restricter
 type Restricter interface {
 	Restrict(ctx context.Context, uid int, keys []string) (map[string][]byte, error)
-	IDsFromKey(ctx context.Context, uid int, mid int, key string) ([]int, error)
-	IDsFromCollection(ctx context.Context, uid int, mid int, collection string) ([]int, error)
+	IDsFromKey(ctx context.Context, uid int, key string) ([]int, error)
 }
