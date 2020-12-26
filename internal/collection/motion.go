83b2a238053c047015bc6a1d0255bed2b667273c
Oskar Hahn <mail@oshahn.de>
2020-12-26 21:29:36 +0100

    Motion (#74)
    Fixes #37
    Fixes #36

--- internal/collection/motion.go
+++ internal/collection/motion.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"strconv"
 
 	"github.com/OpenSlides/openslides-permission-service/internal/dataprovider"
 	"github.com/OpenSlides/openslides-permission-service/internal/perm"
@@ -23,59 +24,116 @@ func NewMotion(dp dataprovider.DataProvider) *Motion {
 
 // Connect registers the Motion handlers.
 func (m *Motion) Connect(s perm.HandlerStore) {
-	s.RegisterWriteHandler("motion.set_state", perm.WriteCheckerFunc(m.setState))
+	s.RegisterWriteHandler("motion.delete", m.modify("motion.can_manage"))
+	s.RegisterWriteHandler("motion.set_state", m.modify("motion.can_manage_metadata"))
+	s.RegisterWriteHandler("motion.create", m.create())
 }
 
-func (m *Motion) setState(ctx context.Context, userID int, payload map[string]json.RawMessage) (map[string]interface{}, error) {
-	motionFQID := fmt.Sprintf("motion/%s", payload["id"])
-	meetingID, err := m.dp.MeetingFromModel(ctx, motionFQID)
-	if err != nil {
-		return nil, fmt.Errorf("getting meeting for %s: %w", motionFQID, err)
+func (m *Motion) create() perm.WriteCheckerFunc {
+	allowList := map[string]bool{
+		"title":                true,
+		"text":                 true,
+		"reason":               true,
+		"category_id":          true,
+		"statute_paragraph_id": true,
+		"workflow_id":          true,
+		"meeting_id":           true,
 	}
 
-	isManager, err := perm.IsAllowed(perm.EnsurePerm(ctx, m.dp, userID, meetingID, "motion.can_manage_metadata"))
-	if err != nil {
-		return nil, fmt.Errorf("checking meta manager permission: %w", err)
+	allowListAmendment := map[string]bool{
+		"parent_id":            true,
+		"amendment_paragraphs": true,
 	}
 
-	if isManager {
+	for k := range allowList {
+		allowListAmendment[k] = true
+	}
+
+	return func(ctx context.Context, userID int, payload map[string]json.RawMessage) (map[string]interface{}, error) {
+		meetingID, _ := strconv.Atoi(string(payload["meeting_id"]))
+
+		perms, err := perm.Perms(ctx, userID, meetingID, m.dp)
+		if err != nil {
+			return nil, fmt.Errorf("fetching perms: %w", err)
+		}
+
+		if perms.HasOne("motion.can_manage") {
+			return nil, nil
+		}
+
+		requiredPerm := "motion.can_create"
+		aList := allowList
+		if _, ok := payload["parent_id"]; ok {
+			requiredPerm = "motion.can_create_amendment"
+			aList = allowListAmendment
+		}
+
+		if !perms.HasOne(requiredPerm) {
+			return nil, perm.NotAllowedf("User %d does not have permission %s", userID, requiredPerm)
+		}
+
+		for e := range payload {
+			if !aList[string(e)] {
+				return nil, perm.NotAllowedf("Field `%s` is forbidden for non manager.", e)
+			}
+		}
+
 		return nil, nil
 	}
+}
 
-	var submitterIDs []int
-	if err := m.dp.Get(ctx, motionFQID+"/submitter_ids", &submitterIDs); err != nil {
-		return nil, fmt.Errorf("getting submitter ids: %w", err)
-	}
-
-	var isSubmitter bool
-	for _, sid := range submitterIDs {
-		var sUserID int
-		if err := m.dp.Get(ctx, fmt.Sprintf("motion_submitter/%d/user_id", sid), &sUserID); err != nil {
-			return nil, fmt.Errorf("getting userid of sumitter %d: %w", sid, err)
+func (m *Motion) modify(managePerm string) perm.WriteCheckerFunc {
+	return func(ctx context.Context, userID int, payload map[string]json.RawMessage) (map[string]interface{}, error) {
+		motionFQID := fmt.Sprintf("motion/%s", payload["id"])
+		meetingID, err := m.dp.MeetingFromModel(ctx, motionFQID)
+		if err != nil {
+			return nil, fmt.Errorf("getting meeting for %s: %w", motionFQID, err)
 		}
-		if sUserID == userID {
-			isSubmitter = true
-			break
+
+		isManager, err := perm.IsAllowed(perm.EnsurePerm(ctx, m.dp, userID, meetingID, managePerm))
+		if err != nil {
+			return nil, fmt.Errorf("checking meta manager permission: %w", err)
 		}
-	}
 
-	if !isSubmitter {
-		return nil, perm.NotAllowedf("User %d is not a manager and not a submitter of %s", userID, motionFQID)
-	}
+		if isManager {
+			return nil, nil
+		}
 
-	var stateID int
-	if err := m.dp.Get(ctx, motionFQID+"/state_id", &stateID); err != nil {
-		return nil, fmt.Errorf("getting stateID: %w", err)
-	}
+		var submitterIDs []int
+		if err := m.dp.Get(ctx, motionFQID+"/submitter_ids", &submitterIDs); err != nil {
+			return nil, fmt.Errorf("getting submitter ids: %w", err)
+		}
 
-	var allowSubmitterEdit bool
-	if err := m.dp.Get(ctx, fmt.Sprintf("motion_state/%d/allow_submitter_edit", stateID), &allowSubmitterEdit); err != nil {
-		return nil, fmt.Errorf("getting allow_submitter_edit: %w", err)
-	}
+		var isSubmitter bool
+		for _, sid := range submitterIDs {
+			var sUserID int
+			if err := m.dp.Get(ctx, fmt.Sprintf("motion_submitter/%d/user_id", sid), &sUserID); err != nil {
+				return nil, fmt.Errorf("getting userid of sumitter %d: %w", sid, err)
+			}
+			if sUserID == userID {
+				isSubmitter = true
+				break
+			}
+		}
 
-	if !allowSubmitterEdit {
-		return nil, perm.NotAllowedf("Motion state does not allow submitter edites")
-	}
+		if !isSubmitter {
+			return nil, perm.NotAllowedf("User %d is not a manager and not a submitter of %s", userID, motionFQID)
+		}
 
-	return nil, nil
+		var stateID int
+		if err := m.dp.Get(ctx, motionFQID+"/state_id", &stateID); err != nil {
+			return nil, fmt.Errorf("getting stateID: %w", err)
+		}
+
+		var allowSubmitterEdit bool
+		if err := m.dp.Get(ctx, fmt.Sprintf("motion_state/%d/allow_submitter_edit", stateID), &allowSubmitterEdit); err != nil {
+			return nil, fmt.Errorf("getting allow_submitter_edit: %w", err)
+		}
+
+		if !allowSubmitterEdit {
+			return nil, perm.NotAllowedf("Motion state does not allow submitter edites")
+		}
+
+		return nil, nil
+	}
 }
--- internal/collection/motion_test.go
+++ internal/collection/motion_test.go
@@ -43,7 +43,7 @@ func TestMotionSetState(t *testing.T) {
 		tdp.Set("motion_state/1/allow_submitter_edit", "true")
 
 		if _, err := setState.IsAllowed(context.Background(), 1, payload); err != nil {
-			t.Errorf("Got unexpected error: %w", err)
+			t.Errorf("Got unexpected error: %v", err)
 		}
 	})
 
@@ -58,7 +58,144 @@ func TestMotionSetState(t *testing.T) {
 		tdp.AddPermissionToGroup(1, "motion.can_manage_metadata")
 
 		if _, err := setState.IsAllowed(context.Background(), 1, payload); err != nil {
-			t.Errorf("Got unexpected error: %w", err)
+			t.Errorf("Got unexpected error: %v", err)
+		}
+	})
+}
+
+func TestMotionDelete(t *testing.T) {
+	tdp := tests.NewTestDataProvider()
+	tdp.AddUserToMeeting(1, 1)
+
+	tdp.AddBasicModel("motion", 1)
+	tdp.Set("motion/1/submitter_ids", "[1]")
+	tdp.Set("motion/1/state_id", "1")
+
+	tdp.AddBasicModel("motion_submitter", 1)
+	tdp.Set("motion_submitter/1/user_id", "1")
+	tdp.Set("motion_submitter/1/motion_id", "1")
+
+	tdp.AddBasicModel("motion_state", 1)
+	tdp.Set("motion_state/1/motion_ids", "[1]")
+	tdp.Set("motion_state/1/allow_submitter_edit", "true")
+
+	dp := dataprovider.DataProvider{External: tdp}
+	m := user.NewMotion(dp)
+	hs := new(tests.HandlerStoreMock)
+	m.Connect(hs)
+	delete, ok := hs.WriteHandler["motion.delete"]
+	if !ok {
+		t.Fatalf("Unknown handler `motion.delete`")
+	}
+
+	payload := map[string]json.RawMessage{
+		"id": []byte("1"),
+	}
+
+	t.Run("correct state", func(t *testing.T) {
+		tdp.Set("motion_state/1/allow_submitter_edit", "true")
+
+		if _, err := delete.IsAllowed(context.Background(), 1, payload); err != nil {
+			t.Errorf("Got unexpected error: %v", err)
+		}
+	})
+
+	t.Run("wrong state", func(t *testing.T) {
+		tdp.Set("motion_state/1/allow_submitter_edit", "false")
+		_, err := delete.IsAllowed(context.Background(), 1, payload)
+		assertNotAllowed(t, err)
+	})
+
+	t.Run("manager", func(t *testing.T) {
+		tdp.Set("motion_state/1/allow_submitter_edit", "false")
+		tdp.AddPermissionToGroup(1, "motion.can_manage")
+
+		if _, err := delete.IsAllowed(context.Background(), 1, payload); err != nil {
+			t.Errorf("Got unexpected error: %v", err)
+		}
+	})
+}
+
+func TestMotionCreate(t *testing.T) {
+	tdp := tests.NewTestDataProvider()
+	tdp.AddUserToMeeting(1, 1)
+
+	dp := dataprovider.DataProvider{External: tdp}
+	m := user.NewMotion(dp)
+	hs := new(tests.HandlerStoreMock)
+	m.Connect(hs)
+	create, ok := hs.WriteHandler["motion.create"]
+	if !ok {
+		t.Fatalf("Unknown handler `motion.create`")
+	}
+
+	t.Run("create simple fields without perm", func(t *testing.T) {
+		tdp.Set("group/1/permissions", "[]")
+		payload := map[string]json.RawMessage{
+			"meeting_id": []byte("1"),
+		}
+
+		_, err := create.IsAllowed(context.Background(), 1, payload)
+		assertNotAllowed(t, err)
+
+	})
+
+	t.Run("create simple fields with perm", func(t *testing.T) {
+		tdp.Set("group/1/permissions", `["motion.can_create"]`)
+		payload := map[string]json.RawMessage{
+			"meeting_id": []byte("1"),
+		}
+
+		if _, err := create.IsAllowed(context.Background(), 1, payload); err != nil {
+			t.Errorf("got unexpected error: %v", err)
+		}
+	})
+
+	t.Run("create amentment fields with wrong perm", func(t *testing.T) {
+		tdp.Set("group/1/permissions", `["motion.can_create"]`)
+		payload := map[string]json.RawMessage{
+			"meeting_id": []byte("1"),
+			"parent_id":  []byte("1"),
+		}
+
+		_, err := create.IsAllowed(context.Background(), 1, payload)
+		assertNotAllowed(t, err)
+
+	})
+
+	t.Run("create amentment fields with perm", func(t *testing.T) {
+		tdp.Set("group/1/permissions", `["motion.can_create_amendment"]`)
+		payload := map[string]json.RawMessage{
+			"meeting_id": []byte("1"),
+			"parent_id":  []byte("1"),
+		}
+
+		if _, err := create.IsAllowed(context.Background(), 1, payload); err != nil {
+			t.Errorf("got unexpected error: %v", err)
+		}
+	})
+
+	t.Run("create privileg fields with can_create", func(t *testing.T) {
+		tdp.Set("group/1/permissions", `["motion.can_create"]`)
+		payload := map[string]json.RawMessage{
+			"meeting_id":    []byte("1"),
+			"agenda_create": []byte("true"),
+		}
+
+		_, err := create.IsAllowed(context.Background(), 1, payload)
+		assertNotAllowed(t, err)
+
+	})
+
+	t.Run("create privileg fields with can_manage", func(t *testing.T) {
+		tdp.Set("group/1/permissions", `["motion.can_manage"]`)
+		payload := map[string]json.RawMessage{
+			"meeting_id":    []byte("1"),
+			"agenda_create": []byte("true"),
+		}
+
+		if _, err := create.IsAllowed(context.Background(), 1, payload); err != nil {
+			t.Errorf("got unexpected error: %v", err)
 		}
 	})
 }
