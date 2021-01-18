08c7b9c9c35fffc729751d3f0b605259e4e031bb
Oskar Hahn <mail@oshahn.de>
2021-01-18 23:54:47 +0100

    Update permission service (#150)

--- internal/autoupdate/mock_test.go
+++ internal/autoupdate/mock_test.go
@@ -1,26 +1,15 @@
 package autoupdate_test
 
 import (
-	"context"
-	"encoding/json"
-
 	"github.com/openslides/openslides-autoupdate-service/internal/autoupdate"
 	"github.com/openslides/openslides-autoupdate-service/internal/test"
 )
 
 func getConnection(closed <-chan struct{}) (*autoupdate.Connection, *test.MockDatastore) {
 	datastore := new(test.MockDatastore)
-	s := autoupdate.New(datastore, new(test.MockRestricter), mockUserUpdater{}, closed)
+	s := autoupdate.New(datastore, new(test.MockRestricter), test.UserUpdater{}, closed)
 	kb := test.KeysBuilder{K: test.Str("user/1/name")}
 	c := s.Connect(1, kb)
 
 	return c, datastore
-}
-
-type mockUserUpdater struct {
-	userIDs []int
-}
-
-func (u mockUserUpdater) AdditionalUpdate(ctx context.Context, updated map[string]json.RawMessage) ([]int, error) {
-	return u.userIDs, nil
 }
