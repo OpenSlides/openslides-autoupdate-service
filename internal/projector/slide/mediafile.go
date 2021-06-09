7d5a72b849eee976f890b700115f074d2e4e0458
Ralf Peschke <rpeschke@peschke-it.de>
2021-06-09 15:32:36 +0200

    Projector.service slide Mediafile

--- internal/projector/slide/mediafile.go
+++ internal/projector/slide/mediafile.go
@@ -9,10 +9,43 @@ import (
 	"github.com/OpenSlides/openslides-autoupdate-service/pkg/datastore"
 )
 
+type dbMediafile struct {
+	ID       int    `json:"id"`
+	Mimetype string `json:"mimetype"`
+}
+
+func mediafileItemFromMap(in map[string]json.RawMessage) (*dbMediafile, error) {
+	bs, err := json.Marshal(in)
+	if err != nil {
+		return nil, fmt.Errorf("encoding mediafile data")
+	}
+
+	var mf dbMediafile
+	if err := json.Unmarshal(bs, &mf); err != nil {
+		return nil, fmt.Errorf("decoding mediafile item: %w", err)
+	}
+	return &mf, nil
+}
+
 // Mediafile renders the mediafile slide.
 func Mediafile(store *projector.SlideStore) {
 	store.RegisterSliderFunc("mediafile", func(ctx context.Context, ds projector.Datastore, p7on *projector.Projection) (encoded []byte, keys []string, err error) {
-		return []byte(`"TODO"`), nil, nil
+		fetch := datastore.NewFetcher(ds)
+		defer func() {
+			if err == nil {
+				err = fetch.Error()
+			}
+		}()
+		data := fetch.Object(ctx, []string{"id", "mimetype"}, p7on.ContentObjectID)
+		mediafile, err := mediafileItemFromMap(data)
+		if err != nil {
+			return nil, nil, err
+		}
+		responseValue, err := json.Marshal(map[string]interface{}{"id": mediafile.ID, "mimetype": mediafile.Mimetype})
+		if err != nil {
+			return nil, nil, err
+		}
+		return responseValue, fetch.Keys(), err
 	})
 
 	store.RegisterGetTitleInformationFunc("mediafile", func(ctx context.Context, fetch *datastore.Fetcher, fqid string, itemNumber string) (json.RawMessage, error) {
