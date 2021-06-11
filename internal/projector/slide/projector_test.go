6cf20589d619f212acda969ef83472613df927e2
Ralf Peschke <rpeschke@peschke-it.de>
2021-06-11 13:54:38 +0200

    Rebased

--- internal/projector/slide/projector_test.go
+++ internal/projector/slide/projector_test.go
@@ -14,7 +14,7 @@ func TestProjectorCountdown(t *testing.T) {
 	s := new(projector.SlideStore)
 	slide.ProjectorCountdown(s)
 
-	pcSlide := s.Get("projector_countdown")
+	pcSlide := s.GetSlideFunc("projector_countdown")
 	assert.NotNilf(t, pcSlide, "Slide with name `projector_countdown` not found.")
 
 	data := dsmock.YAMLData(`
@@ -71,7 +71,7 @@ func TestProjectorMessage(t *testing.T) {
 	s := new(projector.SlideStore)
 	slide.ProjectorMessage(s)
 
-	pmSlide := s.Get("projector_message")
+	pmSlide := s.GetSlideFunc("projector_message")
 	assert.NotNilf(t, pmSlide, "Slide with name `projector_message` not found.")
 
 	data := dsmock.YAMLData(`
