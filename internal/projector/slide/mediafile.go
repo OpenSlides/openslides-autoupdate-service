5f51233d3f5d34d8edc4962287a14d477c7e4ba6
Ralf Peschke <github@peschke-it.de>
2021-06-28 12:13:15 +0200

    Projector.service mediafile based on agenda item list (#224)
    with mediafile.GetTitleInformation

--- internal/projector/slide/mediafile.go
+++ internal/projector/slide/mediafile.go
@@ -9,22 +9,64 @@ import (
 	"github.com/OpenSlides/openslides-autoupdate-service/pkg/datastore"
 )
 
+type dbMediafile struct {
+	ID       int    `json:"id"`
+	Title    string `json:"title"`
+	Mimetype string `json:"mimetype"`
+}
+
+func mediafileItemFromMap(in map[string]json.RawMessage) (*dbMediafile, error) {
+	bs, err := json.Marshal(in)
+	if err != nil {
+		return nil, fmt.Errorf("encoding mediafile item data: %w", err)
+	}
+
+	var mf dbMediafile
+	if err := json.Unmarshal(bs, &mf); err != nil {
+		return nil, fmt.Errorf("decoding mediafile item data: %w", err)
+	}
+	return &mf, nil
+}
+
 // Mediafile renders the mediafile slide.
 func Mediafile(store *projector.SlideStore) {
 	store.RegisterSliderFunc("mediafile", func(ctx context.Context, ds projector.Datastore, p7on *projector.Projection) (encoded []byte, keys []string, err error) {
-		return []byte(`"TODO"`), nil, nil
+		fetch := datastore.NewFetcher(ds)
+		defer func() {
+			if err == nil {
+				err = fetch.Error()
+			}
+		}()
+		data := fetch.Object(ctx, []string{"id", "mimetype"}, p7on.ContentObjectID)
+		mediafile, err := mediafileItemFromMap(data)
+		if err != nil {
+			return nil, nil, fmt.Errorf("get mediafile item from map: %w", err)
+		}
+		responseValue, err := json.Marshal(map[string]interface{}{"id": mediafile.ID, "mimetype": mediafile.Mimetype})
+		if err != nil {
+			return nil, nil, fmt.Errorf("encoding response slide mediafile item: %w", err)
+		}
+		return responseValue, fetch.Keys(), err
 	})
 
 	store.RegisterGetTitleInformationFunc("mediafile", func(ctx context.Context, fetch *datastore.Fetcher, fqid string, itemNumber string) (json.RawMessage, error) {
-		title := "title of mediafile (TODO)"
-
-		agendatitle := struct {
-			Title string `json:"title"`
-		}{
-			title,
+		data := fetch.Object(ctx, []string{"id", "title"}, fqid)
+		mediafile, err := mediafileItemFromMap(data)
+		if err != nil {
+			return nil, fmt.Errorf("get mediafile: %w", err)
 		}
 
-		bs, err := json.Marshal(agendatitle)
+		mediafiletitle := struct {
+			Collection      string `json:"collection"`
+			ContentObjectID string `json:"content_object_id"`
+			Title           string `json:"title"`
+		}{
+			"mediafile",
+			fqid,
+			mediafile.Title,
+		}
+
+		bs, err := json.Marshal(mediafiletitle)
 		if err != nil {
 			return nil, fmt.Errorf("decoding title: %w", err)
 		}
