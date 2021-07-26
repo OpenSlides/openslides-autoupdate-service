614ba9652c8d5e154d1969f235ec1c261467d847
Ralf Peschke <github@peschke-it.de>
2021-07-26 13:05:07 +0200

    Projector.service: Remove agenda_item slide and GetTitleInformation (#269)
    Co-authored-by: Ralf Peschke <rpeschke@peschke-it.de>

--- internal/projector/slide/agenda.go
+++ internal/projector/slide/agenda.go
@@ -62,60 +62,6 @@ type outAgendaItem struct {
 	id               int
 }
 
-// AgendaItem renders the agenda_item slide.
-func AgendaItem(store *projector.SlideStore) {
-	store.RegisterSliderFunc("agenda_item", func(ctx context.Context, ds projector.Datastore, p7on *projector.Projection) (encoded []byte, keys []string, err error) {
-		fetch := datastore.NewFetcher(ds)
-		defer func() {
-			if err == nil {
-				err = fetch.Error()
-			}
-		}()
-
-		data := fetch.Object(
-			ctx,
-			[]string{
-				"id",
-				"item_number",
-				"content_object_id",
-				"meeting_id",
-				"is_hidden",
-				"is_internal",
-				"weight",
-				"level",
-			},
-			p7on.ContentObjectID,
-		)
-
-		agendaItem, err := agendaItemFromMap(data)
-		if err != nil {
-			return nil, nil, fmt.Errorf("get agenda item: %w", err)
-		}
-
-		collection := strings.Split(agendaItem.ContentObjectID, "/")[0]
-		titler := store.GetTitleInformationFunc(collection)
-		if titler == nil {
-			return nil, nil, fmt.Errorf("no titler function registered for %s", collection)
-		}
-
-		titleInfo, err := titler.GetTitleInformation(ctx, fetch, agendaItem.ContentObjectID, agendaItem.ItemNumber, p7on.MeetingID)
-		if err != nil {
-			return nil, nil, fmt.Errorf("get title func: %w", err)
-		}
-
-		out := outAgendaItem{
-			TitleInformation: titleInfo,
-			Depth:            agendaItem.Depth,
-		}
-
-		responseValue, err := json.Marshal(out)
-		if err != nil {
-			return nil, nil, fmt.Errorf("encoding response slide agenda item: %w", err)
-		}
-		return responseValue, fetch.Keys(), err
-	})
-}
-
 // AgendaItemList renders the agenda_item_list slide.
 func AgendaItemList(store *projector.SlideStore) {
 	store.RegisterSliderFunc("agenda_item_list", func(ctx context.Context, ds projector.Datastore, p7on *projector.Projection) (encoded []byte, keys []string, err error) {
