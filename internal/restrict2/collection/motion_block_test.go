e5e46ecdf0d892a5ecf219257d14ea975893d0b0
Oskar Hahn <mail@oshahn.de>
2021-08-13 06:29:20 +0200

    motion block

---
+++ internal/restrict2/collection/motion_block_test.go
@@ -0,0 +1,139 @@
+package collection_test
+
+import (
+	"testing"
+
+	"github.com/OpenSlides/openslides-autoupdate-service/internal/restrict2/collection"
+	"github.com/OpenSlides/openslides-autoupdate-service/internal/restrict2/perm"
+)
+
+func TestMotionBlockModeA(t *testing.T) {
+	f := collection.MotionBlock{}.Modes("A")
+
+	testCase(
+		"no perms",
+		t,
+		f,
+		false,
+		"motion_block/1/id: 1",
+	)
+
+	testCase(
+		"can manage",
+		t,
+		f,
+		true,
+		`---
+		motion_block/1/meeting_id: 1
+		`,
+		withPerms(1, perm.MotionCanManage),
+	)
+
+	testCase(
+		"see agenda item",
+		t,
+		f,
+		true,
+		`---
+		motion_block/1:
+			meeting_id: 1
+			agenda_item_id: 3
+		
+		agenda_item/3/meeting_id: 2
+		`,
+		withPerms(2, perm.AgendaItemCanSee),
+	)
+
+	testCase(
+		"not see agenda item",
+		t,
+		f,
+		false,
+		`---
+		motion_block/1:
+			meeting_id: 1
+			agenda_item_id: 3
+		
+		agenda_item/3/meeting_id: 2
+		`,
+	)
+
+	testCase(
+		"see list of speakers",
+		t,
+		f,
+		true,
+		`---
+		motion_block/1:
+			meeting_id: 1
+			list_of_speakers_id: 3
+		
+		list_of_speakers/3/meeting_id: 2
+		`,
+		withPerms(2, perm.ListOfSpeakersCanSee),
+	)
+
+	testCase(
+		"not see list of speakers",
+		t,
+		f,
+		false,
+		`---
+		motion_block/1:
+			meeting_id: 1
+			list_of_speakers_id: 3
+		
+		list_of_speakers/3/meeting_id: 2
+		`,
+	)
+
+}
+
+func TestMotionBlockModeB(t *testing.T) {
+	f := collection.MotionBlock{}.Modes("B")
+
+	testCase(
+		"no perms",
+		t,
+		f,
+		false,
+		"motion_block/1/id: 1",
+	)
+
+	testCase(
+		"can manage",
+		t,
+		f,
+		true,
+		`---
+		motion_block/1/meeting_id: 1
+		`,
+		withPerms(1, perm.MotionCanManage),
+	)
+
+	testCase(
+		"can see not internal",
+		t,
+		f,
+		true,
+		`---
+		motion_block/1:
+			meeting_id: 1
+			internal: false
+		`,
+		withPerms(1, perm.MotionCanSee),
+	)
+
+	testCase(
+		"can see internal",
+		t,
+		f,
+		false,
+		`---
+		motion_block/1:
+			meeting_id: 1
+			internal: true
+		`,
+		withPerms(1, perm.MotionCanSee),
+	)
+}
