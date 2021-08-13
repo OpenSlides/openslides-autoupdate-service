98e060ba1e71f5e0ea90cd0cd61e67b4d0e02f4d
Oskar Hahn <mail@oshahn.de>
2021-08-13 06:39:23 +0200

    motion change recommendation

---
+++ internal/restrict2/collection/motion_change_recommendation_test.go
@@ -0,0 +1,57 @@
+package collection_test
+
+import (
+	"testing"
+
+	"github.com/OpenSlides/openslides-autoupdate-service/internal/restrict2/collection"
+	"github.com/OpenSlides/openslides-autoupdate-service/internal/restrict2/perm"
+)
+
+func TestMotionChangeRecommendationModeA(t *testing.T) {
+	f := collection.MotionChangeRecommendation{}.Modes("A")
+
+	testCase(
+		"no perms",
+		t,
+		f,
+		false,
+		"motion_change_recommendation/1/id: 1",
+	)
+
+	testCase(
+		"can manage",
+		t,
+		f,
+		true,
+		`---
+		motion_change_recommendation/1/meeting_id: 1
+		`,
+		withPerms(1, perm.MotionCanManage),
+	)
+
+	testCase(
+		"can see not internal",
+		t,
+		f,
+		true,
+		`---
+		motion_change_recommendation/1:
+			meeting_id: 1
+			internal: false
+		`,
+		withPerms(1, perm.MotionCanSee),
+	)
+
+	testCase(
+		"can see internal",
+		t,
+		f,
+		false,
+		`---
+		motion_change_recommendation/1:
+			meeting_id: 1
+			internal: true
+		`,
+		withPerms(1, perm.MotionCanSee),
+	)
+}
