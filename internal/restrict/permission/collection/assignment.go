d837515743d6df5b25fe7a7fa533e152de158264
Oskar Hahn <mail@oshahn.de>
2021-08-13 11:10:13 +0200

    Delete old restricter

--- internal/restrict/permission/collection/assignment.go
+++
@@ -1,9 +0,0 @@
-package collection
-
-import (
-	"github.com/OpenSlides/openslides-autoupdate-service/internal/restrict/permission/perm"
-)
-
-func canSeeAssignmentCandidate(p *perm.Permission) bool {
-	return p.Has(perm.AssignmentCanSee)
-}
