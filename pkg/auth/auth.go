9d9be00bad01f9e72e1464b37c3ff5936d1388fc
Oskar Hahn <mail@oshahn.de>
2022-01-08 21:18:37 +0100

    cookie header

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -232,7 +232,8 @@ func (a *Auth) refreshToken(ctx context.Context, token, cookie string) (string, 
 	}
 
 	req.Header.Add(authHeader, "bearer "+token)
-	req.AddCookie(&http.Cookie{Name: cookieName, Value: "bearer " + cookie})
+	// TODO add header for HttpOnly and Secure
+	req.AddCookie(&http.Cookie{Name: cookieName, Value: "bearer " + cookie, HttpOnly: true, Secure: true})
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
