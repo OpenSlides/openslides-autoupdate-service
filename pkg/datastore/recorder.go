b997d9d1befd2864a3b76b2bc54f68a97eea2195
Oskar Hahn <mail@oshahn.de>
2022-06-08 07:35:33 +0200

    Add new benchmark

--- pkg/datastore/recorder.go
+++ pkg/datastore/recorder.go
@@ -1,6 +1,10 @@
 package datastore
 
-import "context"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+)
 
 // Recorder implements the datastore.Getter interface. It records all requested
 // keys. They can be get with Recorder.Keys().
@@ -28,4 +32,24 @@ func (r *Recorder) Get(ctx context.Context, keys ...Key) (map[Key][]byte, error)
 // Keys returns all datastore keys that where fetched in the process.
 func (r *Recorder) Keys() map[Key]struct{} {
 	return r.keys
+}
+
+// DB creates a json database that contains all values from the recorder.
+func (r *Recorder) DB() ([]byte, error) {
+	keys := make([]Key, 0, len(r.keys))
+	for k := range r.keys {
+		keys = append(keys, k)
+	}
+
+	data, err := r.getter.Get(context.Background(), keys...)
+	if err != nil {
+		return nil, fmt.Errorf("getting all values: %w", err)
+	}
+
+	converted := make(map[string]json.RawMessage, len(data))
+	for k, v := range data {
+		converted[k.String()] = v
+	}
+
+	return json.Marshal(converted)
 }
