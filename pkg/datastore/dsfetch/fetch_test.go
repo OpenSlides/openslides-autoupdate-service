672511e1975012d940225afdab62bcda27df1de4
Oskar Hahn <mail@oshahn.de>
2023-01-16 15:25:36 +0100

    Fix douple lazy

--- pkg/datastore/dsfetch/fetch_test.go
+++ pkg/datastore/dsfetch/fetch_test.go
@@ -58,3 +58,25 @@ func TestFetch_required_field_when_object_does_not_exist(t *testing.T) {
 		t.Errorf("got err `%v`, expected DoesNotExistError", err)
 	}
 }
+
+func TestFech_two_times_lazy(t *testing.T) {
+
+	ds := dsfetch.New(dsmock.Stub(dsmock.YAMLData(`---
+	user/1/username: max
+	`)))
+
+	var username1 string
+	var username2 string
+	ds.User_Username(1).Lazy(&username1)
+	ds.User_Username(1).Lazy(&username2)
+
+	ctx := context.Background()
+
+	if err := ds.Execute(ctx); err != nil {
+		t.Fatalf("Execute: %v", err)
+	}
+
+	if username1 != "max" || username2 != "max" {
+		t.Errorf("usernames: '%s', '%s', expected two times max", username1, username2)
+	}
+}
