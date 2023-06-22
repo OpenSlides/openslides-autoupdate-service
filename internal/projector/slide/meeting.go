6a8ae7d32401a4442b3b91b2fa565cc42ce04397
Luisa <luisa.beerboom@intevation.de>
2023-06-22 11:38:53 +0200

    Implement new WiFi access data slide

--- internal/projector/slide/meeting.go
+++ internal/projector/slide/meeting.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"strconv"
+	"strings"
 
+	"github.com/OpenSlides/openslides-autoupdate-service/internal/projector"
 	"github.com/OpenSlides/openslides-autoupdate-service/internal/projector/datastore"
 )
 
@@ -23,6 +26,9 @@ type dbMeeting struct {
 	ListOfSpeakersAmountNextOnProjector       int    `json:"list_of_speakers_amount_next_on_projector"`
 	ListOfSpeakersAmountLastOnProjector       int    `json:"list_of_speakers_amount_last_on_projector"`
 	ListOfSpeakersShowAmountOfSpeakersOnSlide bool   `json:"list_of_speakers_show_amount_of_speakers_on_slide"`
+	UsersPdfWlanSsid                          string `json:"users_pdf_wlan_ssid"`
+	UsersPdfWlanPassword                      string `json:"users_pdf_wlan_password"`
+	UsersPdfWlanEncryption                    string `json:"users_pdf_wlan_encryption"`
 }
 
 func meetingFromMap(in map[string]json.RawMessage) (*dbMeeting, error) {
@@ -49,4 +55,36 @@ func getMeeting(ctx context.Context, fetch *datastore.Fetcher, meetingID int, fe
 		return nil, fmt.Errorf("get meeting: %w", err)
 	}
 	return meeting, nil
+}
+
+// CurrentSpeakerChyron renders the current_speaker_chyron slide.
+func WiFiAccessData(store *projector.SlideStore) {
+	store.RegisterSliderFunc("wifi_access_data", func(ctx context.Context, fetch *datastore.Fetcher, p7on *projector.Projection) (encoded []byte, err error) {
+		meetingID, err := strconv.Atoi(strings.Split(p7on.ContentObjectID, "/")[1])
+		if err != nil {
+			return nil, fmt.Errorf("error in Atoi with ContentObjectID: %w", err)
+		}
+
+		meeting, err := getMeeting(ctx, fetch, meetingID, []string{
+			"users_pdf_wlan_ssid",
+			"users_pdf_wlan_password",
+			"users_pdf_wlan_encryption",
+		})
+
+		out := struct {
+			UsersPdfWlanSsid       string `json:"users_pdf_wlan_ssid,omitempty"`
+			UsersPdfWlanPassword   string `json:"users_pdf_wlan_password,omitempty"`
+			UsersPdfWlanEncryption string `json:"users_pdf_wlan_encryption,omitempty"`
+		}{
+			meeting.UsersPdfWlanSsid,
+			meeting.UsersPdfWlanPassword,
+			meeting.UsersPdfWlanEncryption,
+		}
+
+		responseValue, err := json.Marshal(out)
+		if err != nil {
+			return nil, fmt.Errorf("encoding response slide current_speaker_chyron: %w", err)
+		}
+		return responseValue, nil
+	})
 }
---
+++ internal/projector/slide/meeting_test.go
@@ -0,0 +1,72 @@
+package slide_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/OpenSlides/openslides-autoupdate-service/internal/projector"
+	"github.com/OpenSlides/openslides-autoupdate-service/internal/projector/datastore"
+	"github.com/OpenSlides/openslides-autoupdate-service/internal/projector/slide"
+	"github.com/OpenSlides/openslides-autoupdate-service/pkg/datastore/dskey"
+	"github.com/OpenSlides/openslides-autoupdate-service/pkg/datastore/dsmock"
+	"github.com/stretchr/testify/assert"
+	// "github.com/stretchr/testify/require"
+)
+
+func TestMeeting(t *testing.T) {
+	s := new(projector.SlideStore)
+	slide.WiFiAccessData(s)
+
+	wifiSlide := s.GetSlider("wifi_access_data")
+	assert.NotNilf(t, wifiSlide, "Slide with name `wifi_access_data` not found.")
+
+	for _, tt := range []struct {
+		name   string
+		data   map[dskey.Key][]byte
+		expect string
+	}{
+		{
+			"All data filled in",
+			dsmock.YAMLData(`
+			meeting/1:
+				users_pdf_wlan_encryption: WPA
+				users_pdf_wlan_password: Super&StrongP455Word
+				users_pdf_wlan_ssid: RandomWiWi
+			`),
+			`{
+				"users_pdf_wlan_encryption": "WPA",
+				"users_pdf_wlan_password": "Super&StrongP455Word",
+				"users_pdf_wlan_ssid": "RandomWiWi"
+			  }
+			`,
+		},
+		{
+			"Password missing",
+			dsmock.YAMLData(`
+				meeting/1:
+					users_pdf_wlan_encryption: WPA
+					users_pdf_wlan_ssid: RandomWiWi
+			`),
+			`{
+				"users_pdf_wlan_encryption": "WPA",
+				"users_pdf_wlan_ssid": "RandomWiWi"
+			  }
+			`,
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			ds, _ := dsmock.NewMockDatastore(tt.data)
+			fetch := datastore.NewFetcher(ds)
+
+			p7on := &projector.Projection{
+				ContentObjectID: "meeting/1",
+				MeetingID:       1,
+			}
+
+			bs, err := wifiSlide.Slide(context.Background(), fetch, p7on)
+			assert.NoError(t, err)
+			assert.NoError(t, fetch.Err())
+			assert.JSONEq(t, tt.expect, string(bs))
+		})
+	}
+}
