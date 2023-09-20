de4504c7febf6fa6d01c6055c54114427a8a5399
Bastian <me@bastianjoel.de>
2023-09-20 11:40:17 +0200

    Reset hotkeys on projection reset

--- internal/projector/projector.go
+++ internal/projector/projector.go
@@ -54,6 +54,7 @@ func (p *Projector) Reset() {
 	p.mu.Lock()
 	defer p.mu.Unlock()
 	p.cache = make(map[dskey.Key][]byte)
+	p.hotKeys = make(map[dskey.Key]map[dskey.Key]struct{})
 }
 
 // Get is a Getter middleware that passes all keys though but calculates
