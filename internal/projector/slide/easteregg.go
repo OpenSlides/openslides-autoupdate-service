2e224161a47b880d18304556d153b3505a27f7d0
Bastian Rihm <brihm@intevation.de>
2024-02-02 12:17:04 +0100

    Fix linter

--- internal/projector/slide/easteregg.go
+++ internal/projector/slide/easteregg.go
@@ -7,7 +7,7 @@ import (
 	"github.com/OpenSlides/openslides-autoupdate-service/internal/projector/datastore"
 )
 
-// ListOfSpeaker renders current list of speaker slide.
+// Game registers the game slide
 func Game(store *projector.SlideStore) {
 	store.RegisterSliderFunc("game", func(ctx context.Context, fetch *datastore.Fetcher, p7on *projector.Projection) (encoded []byte, err error) {
 		return nil, nil
