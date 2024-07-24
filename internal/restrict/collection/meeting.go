3f3579dae6e7fae571301864bab06afa9c0a9e3b
Oskar Hahn <mail@oshahn.de>
2024-07-24 13:13:33 +0200

    Locked meeting
    Fixes #969

--- internal/restrict/collection/meeting.go
+++ internal/restrict/collection/meeting.go
@@ -18,6 +18,8 @@ import (
 //	The user has the CML can_manage of any meeting and the meeting is a template meeting.
 //	The user has the OML can_manage_organization.
 //
+// If `meeting/locked_from_inside` is set, only users in the meeting can see it.
+//
 // Mode A: Always visible to everyone.
 //
 // Mode B: The user can see the meeting.
@@ -58,31 +60,28 @@ func (m Meeting) see(ctx context.Context, ds *dsfetch.Fetch, meetingIDs ...int) 
 		return nil, fmt.Errorf("getting request user: %w", err)
 	}
 
+	lockedMeetings := make([]bool, len(meetingIDs))
+	enabledAnonymous := make([]bool, len(meetingIDs))
+	for i, id := range meetingIDs {
+		ds.Meeting_LockedFromInside(id).Lazy(&lockedMeetings[i])
+		ds.Meeting_EnableAnonymous(id).Lazy(&enabledAnonymous[i])
+	}
+
+	if err := ds.Execute(ctx); err != nil {
+		return nil, fmt.Errorf("get locked from inside value: %w", err)
+	}
+
 	oml, err := perm.HasOrganizationManagementLevel(ctx, ds, requestUser, perm.OMLCanManageOrganization)
 	if err != nil {
 		return nil, fmt.Errorf("checking organization management level: %w", err)
 	}
 
-	if oml {
-		return meetingIDs, nil
-	}
-
-	return eachCondition(meetingIDs, func(meetingID int) (bool, error) {
-		enableAnonymous, err := ds.Meeting_EnableAnonymous(meetingID).Value(ctx)
-		if err != nil {
-			return false, fmt.Errorf("checking enabled anonymous: %w", err)
-		}
-		if enableAnonymous {
-			return true, nil
-		}
-
-		if requestUser == 0 {
-			return false, nil
-		}
-
+	var allowed []int
+LOOP_MEETINGS:
+	for i, meetingID := range meetingIDs {
 		groupIDs, err := ds.Meeting_GroupIDs(meetingID).Value(ctx)
 		if err != nil {
-			return false, fmt.Errorf("getting group_ids: %w", err)
+			return nil, fmt.Errorf("getting group_ids: %w", err)
 		}
 
 		groupMeetingUserIDs := make([][]int, len(groupIDs))
@@ -90,7 +89,7 @@ func (m Meeting) see(ctx context.Context, ds *dsfetch.Fetch, meetingIDs ...int) 
 			ds.Group_MeetingUserIDs(gID).Lazy(&groupMeetingUserIDs[i])
 		}
 		if err := ds.Execute(ctx); err != nil {
-			return false, fmt.Errorf("fetching meeting_user ids: %w", err)
+			return nil, fmt.Errorf("fetching meeting_user ids: %w", err)
 		}
 
 		groupUserIDList := make([][]int, len(groupMeetingUserIDs))
@@ -103,47 +102,63 @@ func (m Meeting) see(ctx context.Context, ds *dsfetch.Fetch, meetingIDs ...int) 
 
 		}
 		if err := ds.Execute(ctx); err != nil {
-			return false, fmt.Errorf("fetching user ids: %w", err)
+			return nil, fmt.Errorf("fetching user ids: %w", err)
 		}
 
 		for _, userIDs := range groupUserIDList {
 			for _, userID := range userIDs {
 				if requestUser == userID {
-					return true, nil
+					allowed = append(allowed, meetingID)
+					continue LOOP_MEETINGS
 				}
 			}
 		}
 
+		if lockedMeetings[i] {
+			continue
+		}
+
+		if enabledAnonymous[i] || oml {
+			allowed = append(allowed, meetingID)
+			continue
+		}
+
+		if requestUser == 0 {
+			continue
+		}
+
 		committeeID, err := ds.Meeting_CommitteeID(meetingID).Value(ctx)
 		if err != nil {
-			return false, fmt.Errorf("getting committee id of meeting: %w", err)
+			return nil, fmt.Errorf("getting committee id of meeting: %w", err)
 		}
 
 		isCommitteeManager, err := perm.HasCommitteeManagementLevel(ctx, ds, requestUser, committeeID)
 		if err != nil {
-			return false, fmt.Errorf("getting committee management status: %w", err)
+			return nil, fmt.Errorf("getting committee management status: %w", err)
 		}
 
 		if isCommitteeManager {
-			return true, nil
+			allowed = append(allowed, meetingID)
+			continue
 		}
 
 		templateForOrganizationID, err := ds.Meeting_TemplateForOrganizationID(meetingID).Value(ctx)
 		if err != nil {
-			return false, fmt.Errorf("getting template meeting: %w", err)
+			return nil, fmt.Errorf("getting template meeting: %w", err)
 		}
 
 		cmlMeetings, err := perm.ManagementLevelCommittees(ctx, ds, requestUser)
 		if err != nil {
-			return false, fmt.Errorf("getting meetings with cml can manage: %w", err)
+			return nil, fmt.Errorf("getting meetings with cml can manage: %w", err)
 		}
 
 		if !templateForOrganizationID.Null() && len(cmlMeetings) > 0 {
-			return true, nil
+			allowed = append(allowed, meetingID)
+			continue
 		}
+	}
 
-		return false, nil
-	})
+	return allowed, nil
 }
 
 func (m Meeting) modeC(ctx context.Context, ds *dsfetch.Fetch, meetingIDs ...int) ([]int, error) {
