4becd11debb7da7b691c228497ed066a424eb3ca
Oskar Hahn <mail@oshahn.de>
2024-09-11 16:50:20 +0200

    Update projection

--- internal/restrict/collection/projection.go
+++ internal/restrict/collection/projection.go
@@ -10,7 +10,7 @@ import (
 
 // Projection handels the restriction for the projection collection.
 //
-// The user can see a projection, if the user has projector.can_see.
+// The user can see a projection, if he can see the linked projector.
 //
 // Mode A: The user can see the projection.
 type Projection struct{}
@@ -40,5 +40,49 @@ func (p Projection) Modes(mode string) FieldRestricter {
 }
 
 func (p Projection) see(ctx context.Context, ds *dsfetch.Fetch, projectionIDs ...int) ([]int, error) {
-	return meetingPerm(ctx, ds, p, projectionIDs, perm.ProjectorCanSee)
+	projectorRestrictor := Collection(ctx, "projector").Modes("A")
+
+	return eachMeeting(ctx, ds, p, projectionIDs, func(meetingID int, ids []int) ([]int, error) {
+		perms, err := perm.FromContext(ctx, meetingID)
+		if err != nil {
+			return nil, fmt.Errorf("getting permission: %w", err)
+		}
+
+		if perms.Has(perm.ProjectorCanManage) {
+			return ids, nil
+		}
+
+		current_projector := make([]dsfetch.Maybe[int], len(ids))
+		for i, id := range ids {
+			ds.Projection_CurrentProjectorID(id).Lazy(&current_projector[i])
+		}
+
+		if err := ds.Execute(ctx); err != nil {
+			return nil, fmt.Errorf("reading current_projector_id")
+		}
+
+		var allowed []int
+		for i, maybeID := range current_projector {
+			projectorID, hasCurrent := maybeID.Value()
+			if !hasCurrent {
+				continue
+			}
+
+			// This chekcs each projector by its own. But the result should be
+			// in the cache anyway. So this should be more performent, then
+			// putting many projector-ids in a set.
+			canSeeProjector, err := projectorRestrictor(ctx, ds, projectorID)
+			if err != nil {
+				return nil, fmt.Errorf("checking projector restrictor: %w", err)
+			}
+
+			if len(canSeeProjector) == 0 {
+				continue
+			}
+
+			allowed = append(allowed, ids[i])
+		}
+
+		return allowed, nil
+	})
 }
