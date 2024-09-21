c920cd6c307834f0e0df1e2ae1beeda5001f32ee
Oskar Hahn <mail@oshahn.de>
2024-09-21 09:13:05 +0200

    Add new gender collection
    # TODO: add it to restrit.go/collectionOrder

---
+++ internal/restrict/collection/gender.go
@@ -0,0 +1,29 @@
+package collection
+
+import (
+	"context"
+
+	"github.com/OpenSlides/openslides-autoupdate-service/pkg/datastore/dsfetch"
+)
+
+// Gender handles permission for action_worker.
+type Gender struct{}
+
+// Name returns the collection name.
+func (a Gender) Name() string {
+	return "gender"
+}
+
+// MeetingID returns no meeting.
+func (a Gender) MeetingID(ctx context.Context, ds *dsfetch.Fetch, id int) (int, bool, error) {
+	return 0, false, nil
+}
+
+// Modes returns the restrictions modes for the action_worker collection.
+func (a Gender) Modes(mode string) FieldRestricter {
+	switch mode {
+	case "A":
+		return Allways
+	}
+	return nil
+}
