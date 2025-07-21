8be088bd438c18e6723a1cc0ca46caa39d89a9e9
Jan Malte Behrens <janmtbehrens@gmail.com>
2025-07-21 15:29:06 +0200

    Added dev_timeout functionality for running and testing go build

--- dev/command.sh
+++ dev/command.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 # Timeout argument to be used in GitHub Workflows
 
-if [ "$APP_CONTEXT" = "dev"   ]
+if [ "$APP_CONTEXT" = "dev" ]
 then
-    CompileDaemon -log-prefix=false -build="go build" -command="./openslides-autoupdate-service"
+    if [ -n "$DEV_TIMEOUT" ]
+    then
+        echo "Go build and wait for $DEV_TIMEOUT seconds"
+        CompileDaemon -log-prefix=false -build="go build" -command="./openslides-autoupdate-service" &
+
+        # Sleep for 15 seconds, then check if build executable exists
+        sleep "$DEV_TIMEOUT"
+
+        if [ -f "./openslides-autoupdate-service" ]; then echo "Found go executable" && exit 0; fi
+        echo "Couldn't find go executable"
+        exit 1
+    else
+        CompileDaemon -log-prefix=false -build="go build" -command="./openslides-autoupdate-service"
+    fi
 fi
 if [ "$APP_CONTEXT" = "tests" ]; then sleep inf; fi
