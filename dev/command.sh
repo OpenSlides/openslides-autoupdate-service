0b100355174d20cf9ad217b5eb222b9db32bf679
Jan Malte Behrens <janmtbehrens@gmail.com>
2025-07-21 14:34:17 +0200

    Further workflow tests

--- dev/command.sh
+++ dev/command.sh
@@ -3,11 +3,6 @@
 
 if [ "$APP_CONTEXT" = "dev"   ]
 then
-    if [ -n "$TIMEOUT_DEV" ]
-    then
-        timeout --preserve-status --signal=SIGINT 5s CompileDaemon -log-prefix=false -build="go build" -command="./openslides-autoupdate-service"
-    else
-        CompileDaemon -log-prefix=false -build="go build" -command="./openslides-autoupdate-service"
-    fi
+    CompileDaemon -log-prefix=false -build="go build" -command="./openslides-autoupdate-service"
 fi
 if [ "$APP_CONTEXT" = "tests" ]; then sleep inf; fi
