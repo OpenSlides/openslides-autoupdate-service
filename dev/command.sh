27baa980b0039afa65041e803536efc7d995e693
Jan Malte Behrens <janmtbehrens@gmail.com>
2025-09-02 14:11:55 +0200

    Transfer PID to CompileDaemon in command.sh

--- dev/command.sh
+++ dev/command.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Timeout argument to be used in GitHub Workflows
 
-if [ "$APP_CONTEXT" = "dev" ]; then CompileDaemon -log-prefix=false -build="go build" -command="./openslides-autoupdate-service"; fi
+if [ "$APP_CONTEXT" = "dev" ]; then exec CompileDaemon -log-prefix=false -build="go build" -command="./openslides-autoupdate-service"; fi
 if [ "$APP_CONTEXT" = "tests" ]; then sleep inf; fi
