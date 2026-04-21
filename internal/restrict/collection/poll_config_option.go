115a5546410bef7344285069c674a9a02b704659
Bastian Rihm <brihm@intevation.de>
2026-04-21 11:47:01 +0200

    New poll option format

--- internal/restrict/collection/poll_config_option.go
+++ internal/restrict/collection/poll_config_option.go
@@ -3,8 +3,6 @@ package collection
 import (
 	"context"
 	"fmt"
-	"strconv"
-	"strings"
 
 	"github.com/OpenSlides/openslides-go/datastore/dsfetch"
 )
@@ -21,7 +19,7 @@ func (a PollConfigOption) Name() string {
 
 // MeetingID returns the meeting of the poll
 func (a PollConfigOption) MeetingID(ctx context.Context, ds *dsfetch.Fetch, id int) (int, bool, error) {
-	pollID, err := a.pollID(ctx, ds, id)
+	pollID, err := ds.PollOption_PollID(id).Value(ctx)
 	if err != nil {
 		return 0, false, fmt.Errorf("getting poll id: %w", err)
 	}
@@ -44,7 +42,7 @@ func (a PollConfigOption) Modes(mode string) FieldRestricter {
 
 func (a PollConfigOption) see(ctx context.Context, ds *dsfetch.Fetch, pollConfigApprovalIDs ...int) ([]int, error) {
 	return eachCondition(pollConfigApprovalIDs, func(pollConfigApprovalID int) (bool, error) {
-		pollID, err := a.pollID(ctx, ds, pollConfigApprovalID)
+		pollID, err := ds.PollOption_PollID(pollConfigApprovalID).Value(ctx)
 		if err != nil {
 			return false, fmt.Errorf("getting poll id: %w", err)
 		}
@@ -60,42 +58,4 @@ func (a PollConfigOption) see(ctx context.Context, ds *dsfetch.Fetch, pollConfig
 
 		return false, nil
 	})
-}
-
-func (a PollConfigOption) pollID(ctx context.Context, ds *dsfetch.Fetch, pollConfigOptionID int) (int, error) {
-	config, err := ds.PollConfigOption_PollConfigID(pollConfigOptionID).Value(ctx)
-	if err != nil {
-		return 0, fmt.Errorf("getting poll config: %w", err)
-	}
-
-	collection, configIDStr, found := strings.Cut(config, "/")
-	if !found {
-		return 0, fmt.Errorf("invalid config %s", config)
-	}
-
-	configID, err := strconv.Atoi(configIDStr)
-	if err != nil {
-		return 0, fmt.Errorf("invalid config id %s", config)
-	}
-
-	var field func(int) *dsfetch.ValueInt
-	switch collection {
-	case "poll_config_approval":
-		field = ds.PollConfigApproval_PollID
-	case "poll_config_selection":
-		field = ds.PollConfigSelection_PollID
-	case "poll_config_rating_score":
-		field = ds.PollConfigRatingScore_PollID
-	case "poll_config_rating_approval":
-		field = ds.PollConfigRatingApproval_PollID
-	default:
-		return 0, fmt.Errorf("unknown poll config collection %s", collection)
-	}
-
-	pollID, err := field(configID).Value(ctx)
-	if err != nil {
-		return 0, fmt.Errorf("getting poll id: %w", err)
-	}
-
-	return pollID, nil
 }
